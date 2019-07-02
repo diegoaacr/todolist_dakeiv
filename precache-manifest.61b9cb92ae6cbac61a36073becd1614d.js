1db61517b6599cb08ced70d5615f721c8aded36c
Diego Cueva <diegocueva.re@gmail.com>
2019-07-02 18:35:30 -0500

    Updates

---
+++ precache-manifest.61b9cb92ae6cbac61a36073becd1614d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2add8912aae5b4b07cea551f79205fd7",
+    "url": "/todolist_dakeiv/index.html"
+  },
+  {
+    "revision": "b17e415b4a50ed8d2210",
+    "url": "/todolist_dakeiv/static/css/main.8010303f.chunk.css"
+  },
+  {
+    "revision": "239e7b11c307288b7607",
+    "url": "/todolist_dakeiv/static/js/2.32ae519b.chunk.js"
+  },
+  {
+    "revision": "b17e415b4a50ed8d2210",
+    "url": "/todolist_dakeiv/static/js/main.d1c202ef.chunk.js"
+  },
+  {
+    "revision": "cde5767f5ce8761ca5a4",
+    "url": "/todolist_dakeiv/static/js/runtime~main.e98f073b.js"
+  }
+]);
